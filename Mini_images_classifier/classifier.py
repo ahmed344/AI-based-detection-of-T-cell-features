12434a2b3f3dadf6ffc7f77d08d6684014984822
Ahmed344 <ahmedsharaf483@gmail.com>
2021-01-15 17:36:30 +0100

    add python path

--- Mini_images_classifier/classifier.py
+++ Mini_images_classifier/classifier.py
@@ -1,11 +1,13 @@
+#! /home/ahmed/anaconda3/envs/tf-gpu/bin/python
+
 #Libraries
-import sys
+#import sys
 
 # Utilities
 from classification import Multible_Binary_Classifier
 
 
-classification = Multible_Binary_Classifier(no_of_seeds = 100, no_of_epochs = 5000, norm = 'mean')
+classification = Multible_Binary_Classifier(no_of_seeds = 10, no_of_epochs = 50, norm = 'mean')
 histories = classification.cell(img = "actin1-1.tif",
                                 mask = "Mask.tif",
                                 pattern = "pattern1_25im-1.tif",
