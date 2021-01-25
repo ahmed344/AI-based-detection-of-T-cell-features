d81d8c7245b0cc74d024df74b28ac0fe6775872b
Ahmed344 <ahmedsharaf483@gmail.com>
2021-01-25 17:28:49 +0100

    modify results

--- Mini_images_classifier/classifier.py
+++ Mini_images_classifier/classifier.py
@@ -1,16 +1,19 @@
 #! /home/ahmed/anaconda3/envs/tf-gpu/bin/python
 
 #Libraries
-#import sys
+import sys
 
 # Utilities
 from classification import Multible_Binary_Classifier
 
 
-classification = Multible_Binary_Classifier(no_of_seeds = 10, no_of_epochs = 50, norm = 'mean')
-histories = classification.cell(img = "actin1-1.tif",
+classification = Multible_Binary_Classifier(no_of_seeds = 100, no_of_epochs = 5000, norm = 'mean')
+histories = classification.cell(img = "actin.tif",
                                 mask = "Mask.tif",
-                                pattern = "pattern1_25im-1.tif",
+                                pattern = "pattern.tif",
                                 date = "data/data_400_20190208",
-                                cell = "cell_1_1",
+                                cell = "cell_" + str(sys.argv[1]),
+                                l = 15,
+                                l_min = 0.7,
+                                l_max = 1.3,
                                 img_scale = 10)
--- Mini_images_classifier/data_20190326/results.py
+++ Mini_images_classifier/data_20190326/results.py
@@ -1,15 +1,41 @@
 #! /usr/bin/python
 
+# Libraries
 import os
 import fnmatch
 
-#Collecting the cell folders in a list
+# Collecting the cell directories in a list
 Results = [cell for cell in [ f.name for f in os.scandir() if f.is_dir()] if fnmatch.fnmatch(cell, 'cell_*')]
 
-#creat the folders
-os.mkdir("results")
+# Creat the directories
+try:
+    os.mkdir("results")
+except:
+    print('The directory results already exists')
 
-#make a numpy array for each set of images and create it's results folder
+
+# Make a numpy array for each set of images and create it's results directory
 for result in Results:
-    #creat the folders
-    os.mkdir("results/{}".format(result))
+    # Creat the directories
+    try:
+        os.mkdir("results/{}".format(result))
+    except:
+        print(result, 'directory already exists')
+
+    # Rename the files if they do exist
+    try:
+        os.rename("{}/{}".format(result, 'Results.csv'), "{}/{}".format(result, 'coordinates.csv'))
+    except:
+        print(result, 'Results.csv file does not exist')
+    try:
+        os.rename("{}/{}".format(result, 'actin1-1.tif'), "{}/{}".format(result, 'actin.tif'))
+    except:
+        print(result, 'actin1-1.tif file does not exist')
+    try:
+        os.rename("{}/{}".format(result, 'pattern1_25im-1.tif'), "{}/{}".format(result, 'pattern.tif'))
+    except:
+        print(result, 'pattern1_25im-1.tif file does not exist')
+    try:
+        os.rename("{}/{}".format(result, 'RICM-1.tif'), "{}/{}".format(result, 'ricm.tif'))
+    except:
+        print(result, 'RICM-1.tif file does not exist')
