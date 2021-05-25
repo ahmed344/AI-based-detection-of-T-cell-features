a147d10315f60e407c3a9c3ee5159616af35576c
Ahmed344 <ahmedsharaf483@gmail.com>
2021-05-25 20:10:29 +0200

    radial profile

--- Mini_images_classifier/classifier.py
+++ Mini_images_classifier/classifier.py
@@ -11,7 +11,7 @@ classification = Multible_Binary_Classifier(no_of_seeds = 100, no_of_epochs = 50
 histories = classification.cell(img = "actin.tif",
                                 mask = "Mask.tif",
                                 pattern = "pattern.tif",
-                                date = "data/data_400_20190208",
+                                date = "data/negative_20180306",
                                 cell = "cell_" + str(sys.argv[1]),
                                 l = 15,
                                 l_min = 0.7,
